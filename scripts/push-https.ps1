1cf739d94124030c52c8203f6512e3282642821d
Justin <justobeme@outlook.com>
2025-09-05 07:03:35 +0930

    Automated push

---
+++ scripts/push-https.ps1
@@ -0,0 +1,4 @@
+﻿git remote set-url origin "https://$($env:GITHUB_TOKEN)@github.com/IfIknewher/twoboysAB.git"
+git add .
+git commit -m "Automated push" 2>$null
+git push origin $(git rev-parse --abbrev-ref HEAD)
